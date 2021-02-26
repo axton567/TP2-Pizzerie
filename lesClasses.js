029e51d5ee4876328bd3548c7c0216cba84b5f24
Véronique Poulin <veronique_poulin96@outlook.com>
2021-02-26 13:37:30 -0500

    Création et description des fichiers.

---
+++ lesClasses.js
@@ -0,0 +1,5 @@
+/*
+ * Projet : Les classes destinées à pouvoir passer une commande à une pizzeria.
+ * Auteur : Véronique Poulin et Pierre-Luc Hamel
+ * Date : 26 février 2021
+ */
---
+++ lesFonctions.js
@@ -0,0 +1,5 @@
+/*
+ * Projet : Les fonctions destinées à vérifier les informations entrer pour une commande à une pizzeria.  
+ * Auteur : Véronique Poulin et Pierre-Luc Hamel
+ * Date : 26 février 2021
+ */
