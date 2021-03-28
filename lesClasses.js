b03d5aa035202c3061d9011922b3cdb2042e9f23
Pierre-Luc Hamel <plhamel44@gmail.com>
2021-03-28 13:05:00 -0400

    Correction des if dans lireF et lireG et prixCP

--- lesClasses.js
+++ lesClasses.js
@@ -123,7 +123,7 @@ class Pizza {
             ", Temps de cuisson: " + this.tempsCuisson;
     }
     lireFromage() {
-        if (listeFromagesChoisis == 0) {
+        if (listeFromagesChoisis == "") {
             var s = "";
             for (var i = 0; i < this.fromage.length; i++) {
                 s += "{" + this.fromage[i].lectureFromage() + "}, ";
@@ -141,7 +141,7 @@ class Pizza {
 
     }
     lireGarniture() {
-        if (listeFromagesChoisis == 0) {
+        if (listeFromagesChoisis == "") {
             var s = "";
             for (var i = 0; i < this.garniture.length; i++) {
                 s += "{" + this.garniture[i].lectureGarniture() + "}, ";
@@ -182,7 +182,7 @@ class Pizza {
         this.fromage[this.fromage.length] = fromageSupplémentaire;
     }
     prixCompletPizza() {
-        if (listeFromagesChoisis == 0) {
+        if (listeFromagesChoisis == "") {
             var i = 0;
             var fromage = 0;
             var garniture = 0;
